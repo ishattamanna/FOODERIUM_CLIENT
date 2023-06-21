8d77bece3d932d4b1831fb33feeca7269f5133d0
ishattamanna <ishattamannazinia@gmail.com>
2023-06-21 20:48:47 +0600

    mimo

---
+++ src/pages/Drinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Drinks = () => {
+    return (
+        <div>
+            <h1>All drinks category would be hera</h1>
+        </div>
+    );
+};
+
+export default Drinks;
